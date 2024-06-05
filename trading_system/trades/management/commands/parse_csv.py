1d3c0ff396e738b40c2ed9af1d9555aca5b224ab
Dominador B Ramos Jr. <mongramosjr@gmail.com>
2024-06-05 10:07:47 +0000

    skip header in csv file when processing the bulk order

--- trading_system/trades/management/commands/parse_csv.py
+++ trading_system/trades/management/commands/parse_csv.py
@@ -6,18 +6,23 @@ import csv
 from django.conf import settings
 
 class Command(BaseCommand):
-    help = 'Process bulk trade CSV files from a preconfigured directory'
+    help = 'Process a bulk trade CSV file from a preconfigured directory'
+
+    argument_name = 'filename'
+    argument_description = 'Path to the CSV file containing bulk trades'
+
+    def add_arguments(self, parser):
+        parser.add_argument(self.argument_name, nargs=1, type=str, help=self.argument_description)
+
 
     def handle(self, *args, **kwargs):
+        filename = kwargs.get(self.argument_name)[0]  # Access the first element from the argument list
         bulk_trades_dir = settings.BULK_TRADES_DIR
 
-        # Process each CSV file in the directory
-        for filename in os.listdir(bulk_trades_dir):
-            if filename.endswith('.csv'):
-                file_path = os.path.join(bulk_trades_dir, filename)
-                self.process_csv(file_path)
-                self.stdout.write(self.style.SUCCESS(f'Successfully processed {filename}'))
-
+        file_path = os.path.join(bulk_trades_dir, filename)
+        self.process_csv(file_path)
+        self.stdout.write(self.style.SUCCESS(f'Successfully processed {filename}'))
+        
     def process_csv(self, file_path):
         with open(file_path, mode='r') as csv_file:
             csv_reader = csv.reader(csv_file)
